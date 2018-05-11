172cafb2f264db712cdffe3d84e23385fdf4407a
Alan Raul Arvizu <alan.arvizu@stu.jefferson.kyschools.us>
2018-05-11 08:43:32 -0400

    Initial commit

---
+++ venv/settings.py
@@ -0,0 +1,9 @@
+class Settings():
+    """A class to store all settings for alien invasion."""
+
+    def __init__(self):
+        """initialize the games's settings."""
+        # scree settings
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (230, 230, 230)
